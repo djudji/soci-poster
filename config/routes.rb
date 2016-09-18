a338e590804d8016336a7a120bddd179c63d13c2
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2016-09-18 18:41:26 +0200

    Add Twiter with OmniAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
   root 'pages#home'
+  devise_for :users, controllers: { registrations: 'registrations' }
   get 'dashboard', to: 'pages#dashboard'
+
+  get 'auth/:provider/callback', to: 'connections#create'
+  resources :connections, only: :destroy
 end

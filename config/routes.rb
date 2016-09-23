d1644e6427f60b0332bf2bd2ffc50483a3794193
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2016-09-24 01:46:16 +0200

    Add posts and post validation

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users, controllers: { registrations: 'registrations' }
   get 'dashboard', to: 'pages#dashboard'
+  resources :posts
 
   get 'auth/:provider/callback', to: 'connections#create'
   get 'auth/failure', to: 'connections#omniauth_failure'

c52ebe4ccc649f29bb243a8886721301ea37a3c8
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2016-09-24 15:59:33 +0200

    Add Facebook and Facebook Page posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,4 +15,18 @@ class Post < ApplicationRecord
     end
     client.update(content)
   end
+
+  def to_facebook
+    user_graph = Koala::Facebook::API.new(user.facebook.oauth_token)
+    user_graph.put_connections('me', 'feed', message: content)
+  end
+
+  def to_facebook_page(name = 'Judo Klub Tuzla')
+    user_graph = Koala::Facebook::API.new(user.facebook.oauth_token)
+    pages = user_graph.get_connections('me', 'accounts')
+    fb_page = pages.select { |page| page['name'] == name }
+    page_access_token = fb_page[0]['access_token']
+    page_graph = Koala::Facebook::API.new(page_access_token)
+    page_graph.put_connections('me', 'feed', message: content)
+  end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
-  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], scope: 'email, publish_actions'
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], scope: 'email, public_profile, publish_actions, manage_pages, pages_show_list, pages_messaging, publish_pages, user_managed_groups'
 end
 
 OmniAuth.config.on_failure = proc do |env|
